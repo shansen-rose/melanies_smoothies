cb4e50f8ed8b915846801726f2f344a9ec2f6247
shansen-rose <savanah@autowares.com>
2025-12-19 11:03:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ ingredients_list = st.multiselect(
     , max_selections=6
 )
 
-time_to_insert = st.button('Submit Order')
 
 if ingredients_list:
     ingredients_string = ''
@@ -43,10 +42,13 @@ if ingredients_list:
         my_insert_stmt = """
         insert into smoothies.public.orders(name_on_order, ingredients)
         values ('""" + name_on_order + """', '""" + ingredients_string + """')"""
-       
+
+        time_to_insert = st.button('Submit Order')
     
         if time_to_insert:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+
 
 
