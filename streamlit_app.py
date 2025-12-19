89522a866ea5ae9d2c03169cffae5f6cbaf9e733
shansen-rose <savanah@autowares.com>
2025-12-19 10:16:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 st.title(f"Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
